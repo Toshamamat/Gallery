4f79cd3f3f2ce1e8ae7feb6dedf5ce67cd70691c
Tosha <57236207+Toshamamat@users.noreply.github.com>
2023-04-07 14:30:57 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slide')
+
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        slide.classList.add('active')
+        clearActiveClasses()
+
+        slide.classList.add('active')
+    })
+    
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+        
+    })
+}
